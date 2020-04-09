edc020cbcd94a5157b98d03a9584193ce6f3d9de
edgon06 <55820960+edgon06@users.noreply.github.com>
2020-04-09 16:14:02 -0500

    Actualizando el script de cracion de logins
    Actualizando el script de cracion de logins para ser usado en CMD

--- sqlscript/Creacion_de_logins.sql
+++ sqlscript/Creacion_de_logins.sql
@@ -1,14 +1,13 @@
 USE master
 CREATE login SECRETARIA_LOGIN WITH Password ='1234'
 CREATE login ADMINISTRADOR_LOGIN WITH Password ='123'
-Go
+GO
 Use LuiguiBaretta
-CREATE user Secretaria for Login SECRETARIA_LOGIN
-CREATE user Administrador for Login ADMINISTRADOR_LOGIN
-
+CREATE user Secretaria for LOGIN SECRETARIA_LOGIN
+CREATE user Administrador for LOGIN ADMINISTRADOR_LOGIN
 GO
 GRANT select, update,insert, delete to Secretaria
 GRANT exec to Secretaria
-
 GRANT select, update,insert, delete on ProveerProducto to Administrador
 GRANT select, update,insert, delete on Producto to Administrador
+GO
